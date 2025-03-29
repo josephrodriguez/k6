0e3619ff38e4135c551ab5f2590cbd10d755df93
Jose Rodriguez <4713082+josephrodriguez@users.noreply.github.com>
2025-03-29 15:30:01 +0200

    Add configuration for environment variables for VUs

--- tests/http-load-test.js
+++ tests/http-load-test.js
@@ -1,16 +1,16 @@
 import http from 'k6/http';
 import { check } from 'k6';
 
-const httpBaseUrl = __ENV.K6_BASE_URL || 'http://localhost:8080/';
-
+const httpBaseUrl = __ENV.K6_HTTP_BASE_URL || 'http://localhost:8080/';
+const httpRampUpVirtualUsers = parseInt(__ENV.K6_HTTP_RAMP_UP_VUS || 100, 10);
 const httpLoadDurationSeconds = parseInt(__ENV.K6_HTTP_LOAD_DURATION_SECONDS || 60, 10);
 const httpRampUpDurationSeconds = parseInt(__ENV.K6_HTTP_RAMP_UP_DURATION_SECONDS || 30, 10);
 const httpRampDownDurationSeconds = parseInt(__ENV.K6_HTTP_RAMP_DOWN_DURATION_SECONDS || 30, 10);
 
 export let options = {
   stages: [
-    { duration: `${httpRampUpDurationSeconds}s`, target: 100 },
-    { duration: `${httpLoadDurationSeconds}s`, target: 100 },
+    { duration: `${httpRampUpDurationSeconds}s`, target: httpRampUpVirtualUsers },
+    { duration: `${httpLoadDurationSeconds}s`, target: httpRampUpVirtualUsers },
     { duration: `${httpRampDownDurationSeconds}s`, target: 0 },
   ]
 };
