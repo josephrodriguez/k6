198d8b9adfbdb7cf470369c5d083dddd97db6df8
Jose Rodriguez <4713082+josephrodriguez@users.noreply.github.com>
2025-03-29 16:00:25 +0200

    Pass the prefixed K6 environment variables to K6 test

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,19 @@ echo "k6 Version Information"
 echo "==========================================================="
 k6 version
 echo ""
+
+echo "==========================================================="
+echo "K6 Environment Variables"
+echo "==========================================================="
+ENV_VARS=""
+for var in $(env | grep '^K6_' | cut -d= -f1); do
+    value=$(eval echo \$$var)
+    ENV_VARS="$ENV_VARS -e $var=$value"
+    echo "$var"
+done
+echo ""
+
+echo "==========================================================="
+echo "Running performance test locally with K6"
+echo "==========================================================="
+k6 run $ENV_VARS "${K6_TEST_NAME}.js"
